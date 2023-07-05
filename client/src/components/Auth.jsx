94292f5885aa1a6ef6de7ed0fad525a6dc8697f0
shreeharigowda <89767061+shreeharigowda@users.noreply.github.com>
2023-07-05 22:46:52 +0530

    Update Auth.jsx

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -32,7 +32,7 @@ const Auth = () => {
 
         const { username, password, phoneNumber, avatarURL } = form;
 
-        const URL = 'http://localhost:5000/auth';
+        const URL = 'https://serverchat-hopr.onrender.com/auth';
 
 
         const { data: { token, userId, hashedPassword, fullName } } = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`, {
